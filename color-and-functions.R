77799ffb6f95894eb1def1bdef275d883f917d3c
Sophie Moreau <soanta.moreau@protonmail.com>
2023-06-28 15:44:02 +0200

    all colors for 92 species (no electro data set)

--- color-and-functions.R
+++ color-and-functions.R
@@ -66,7 +66,8 @@ range01 <- function(x) {
   (x-min(x))/(max(x)-min(x))} 
 
 
-lakecolors <- read_rds("/home/sophie/Dokumente/Master Thesis R/Master Thesis Analysis/lakecolors.rds")
+lakecolors <- read_rds("/home/sophie/Dokumente/Master Thesis R/Master Thesis Analysis/df_final_ne.rds")
+lakecolors$Species <- as.factor(lakecolors$Species)
 
 levels(lakecolors$Species)
 lakeColors <- setNames(c(
@@ -77,7 +78,13 @@ lakeColors <- setNames(c(
   "#79AF8B", "#AF6AE2", "#966BFF", "#3B9AB2", "#78B7C5", "#E1AF00", "#1B9E77", 
   "#D95F02", "#7570B3", "#E6AB02", "#66A61E", "#E78AC3", "#A6761D", "#666666", 
   "#5BBCD6", "#1B5E20", "#FD6467", "#FAD510", "#FC8D62", "#8DA0CB", "#550307", 
-  "#A6D854", "#FFD92F"
+  "#A6D854", "#FFD92F", "#ff4316", "#d15550", "#a2688b", "#747ac5", "#458cff", 
+  "#1bff30", "#26e264", "#30c698", "#3ba9cb", "#FFBF7F", "#FF7F00", "#FFFF99", 
+  "#B2FF8C", "#32FF00", "#A5EDFF", "#19B2FF", "#CCBFFF", "#654CFF", "#FF99BF", 
+  "#E51932", "#E64B35", "#4DBBD5", "#00A087", "#3C5488", "#F39B7F", "#8491B4", 
+  "#91D1C2", "#DC0000", "#7E6148", "#B09C85", "#4A6990", "#A73030", "#3B3B3B", 
+  "#8F7700", "#003C67", "#17BECF", "#BCBD22", "#7F7F7F", "#E377C2", "#8C564B", 
+  "#C7C7C7"
 ), levels(lakecolors$Species))
 
 
