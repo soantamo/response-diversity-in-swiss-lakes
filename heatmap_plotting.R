e62c43489c44d0cd3a81eab184f4bb9512de734b
Sophie Moreau <soanta.moreau@protonmail.com>
2023-09-22 14:29:27 +0200

    plot done for biggest Swiss lakes. Honestly no idea how to do it. Confusion

--- heatmap_plotting.R
+++ heatmap_plotting.R
@@ -3,66 +3,80 @@
 library(tidyverse)
 library(readr)
 library(viridis)
+library(corrmorant)
+library(reshape2)
+library(corrplot)
 
+#df with mean values of derivatives per species and lake
 
 df_heatmap_subset <- readRDS("df_heatmap_subset")
 df_heatmap <- readRDS("df_heatmap")
 
-df_subset_wide <-  df_heatmap_subset |>
-  filter(Lake %in% c("Biel", "Brienz")) |> 
+
+
+
+#make dataframe wide for correlation, 4 biggest lakes
+
+deriv_species <- df_heatmap_subset |>
+  filter(Lake %in% c("Geneva", "Neuchatel", "Constance", "Lucerne")) |>
+  # filter(Species %in% c("Alburnus_alburnus", "Coregonus_sp", "Perca_fluviatilis", "Rutilus_rutilus")) |> 
   pivot_wider(names_from = "Species", values_from = "derivative") |> 
-  select(-Lake)
-#make these correlations, dont know how :((
+  select(-Lake, -lake_derivative)
+#   mutate(Geneva = 2.034901) |> 
+#   mutate(Neuchatel = 1.234619) |> 
+#   mutate(Lucerne = 0.05487739) |> 
+#   mutate(Constance = 0.8363541)
+# # 
+# deriv_lakes <-  df_heatmap_subset |>
+#   filter(Lake %in% c("Geneva", "Neuchatel", "Constance", "Lucerne")) |>
+#   pivot_wider(names_from = "Lake", values_from = "lake_derivative") |>
+#   select(-derivative, -Species)
 
-head(df_subset_wide)
 
-#problem is NAs, but we do have a lot of them because species dont occur at the same lakes
-#no correlation possible
-correlation_test <- cor(df_subset_wide, use = "complete.obs", method = "pearson")
+head(deriv_species)
+
+#if two data points per species are only there -> can only be -1 or 1
+#with pairwise.complete.obs the problem of missing values is solved
+#the problem is that I dont need species vs species, but species vs lakes
+
+cor(deriv_species, method = "pearson", use = "pairwise.complete.obs")
+
+COL2(diverging = c("RdBu", "BrBG", "PiYG", "PRGn", "PuOr", "RdYlBu"), n = 200)
+
+corrplot(corr = cor(deriv_species, method = "pearson", use = "pairwise.complete.obs"),
+         method = "color",  na.label = ".", tl.cex = 0.5, tl.col = "black",
+         col = COL2("RdYlBu"))
+
+# type = "lower",
+
+#based on internet
+
+corr_mat <- round(cor(deriv_species, method = "pearson", use = "pairwise.complete.obs"),2) 
+head(corr_mat)
+
+# reduce the size of correlation matrix
+melted_corr_mat <- melt(corr_mat)
+
+# plotting the correlation heatmap
+
+ggplot(data = melted_corr_mat, aes(x = Var1, y = Var2, fill=value)) +
+  geom_tile() +
+  # geom_text(aes(Var2, Var1, label = value),
+  #           color = "white", size = 4) +
+  scale_fill_viridis(option="viridis")
+  # scale_fill_gradient2(low = "white", high = "red")
 
 
-##this works!!, but is it correct?
-cormat <- round(cor(x = as.matrix(df_subset_wide), method = "pearson", use = "pairwise.complete.obs"), 2)
-head(cormat)
+#only working without NAs
+corrmorant(deriv_species)
 
-library(reshape2)
-melted_cormat <- melt(cormat)
-head(melted_cormat)
-
-ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
-  geom_tile()
-
-ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
-  geom_tile(color = "white")+
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal()+ 
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1))+
-  coord_fixed()
-
-suitability_corplot <- corrmorant::corrmorant(as.matrix(d_index_chart), corr_method = "pearson",
-                                              use = "pairwise.complete.obs", style = "binned")+
+suitability_corplot <- corrmorant(deriv_species, corr_method = "pearson", style = "binned")+
   theme(legend.position = "bottom") +
   scale_color_viridis(name = "Correlation", option = "E", direction = -1, limits = c(-1, 1)) +
   theme(axis.text = element_text(size=5),
         axis.text.x = element_text(angle = 45) )
 
-library(corrmorant)
-
-data_numeric <- as.numeric(df_subset_wide)
-
-cor(df_subset_wide, method = "pearson")
-
-suitability_corplot <- corrmorant::corrmorant(df_subset_wide, corr_method = "pearson", style = "binned")+
-  theme(legend.position = "bottom") +
-  scale_color_viridis(name = "Correlation", option = "E", direction = -1, limits = c(-1, 1)) +
-  theme(axis.text = element_text(size=5),
-        axis.text.x = element_text(angle = 45) )
-
-
-
+suitability_corplot
 
 # df_heatmap_subset |> 
 #   ggplot(aes(Lake, Species, fill = derivative)) +
