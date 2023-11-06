fcc70a7a2d97e1fe03e428298a6f9fc0cb1cef1d
Sophie Moreau <soanta.moreau@protonmail.com>
2023-11-06 17:34:36 +0100

    prepared overview for summary of model success and added confidence interval to loop and deleted single testing etc

--- 03-model-2.R
+++ 03-model-2.R
@@ -19,98 +19,6 @@ table(df_abundance_gam$Abundance)
 str(df_abundance_gam)
 head(df_abundance_gam)
 
-#total of nine species
-df_abundance_gam |> 
-  distinct(Species) |> 
-  pull(Species)
-
-
-#model testing for one species
-
-
-df_one <- df_abundance_gam |> 
-  filter(Species ==  "Coregonus_zugensis" )
-
-sum(df_one$Abundance)
-table(df_one$Abundance)
-
-hist(df_one$Abundance)
-
-M1 <- gam(Abundance ~ s(mean_last_7days, k = 7), family = ziP(),
-          data = df_one)
-
-summary.gam(M1)
-gam.check(M1)
-tidy(M1)
-
-#MODEL VALIDATION# M1
-#Synthesis of pdf, GAMM book and GAM book
-
-plot(M1)
-tidy(M1)
-glance(M1)
-
-#1.verifiy homogeneity: rsd vs fitted
-fv <- fitted(M1) ##predicted values
-rsd <- resid(M1) ##residuals
-plot(x = fv, y = rsd, xlab = "Fitted values", ylab = "Residuals")
-
-#ok?
-
-#2.verify model misfit (or independence): rsd vs each covariate in the model and not in the model
-par(mfrow = c(2,2), mar = c(5,5,2,2), cex.lab = 1.5)    
-E1 <- resid(M1)
-F1 <- fitted(M1)
-plot(x=F1, y=E1, xlab = "Fitted values", ylab ="Residuals")
-abline(h=0, lty=2)
-
-plot(x=df_one$mean_last_7days, y = E1, xlab = "Temp", ylab = "Residuals")
-abline(h=0, lty=2)
-
-plot(x=df_one$Protocol, y = E1, xlab = "Protocol", ylab = "Residuals")
-abline(h=0, lty=2)
-
-plot(x=df_one$Depth_sample, y = E1, xlab = "Depth", ylab = "Residuals")
-abline(h=0, lty=2)
-
-#not bad I guess
-
-#3. verifiy independence when multiple measurements were taken over time: auto-correlation
-#not the case here
-
-#4.verifiy normality: histogram of residuals
-
-par(mfrow = c(1,2), mar = c(5, 5, 2, 2))
-hist(E1, main = "", xlab = "Residuals", ylim = c(0,20))
-qqnorm(E1, main = "")
-qqline(E1)
-
-#looks better
-
-#5. check for influental observations: cook distance values
-
-# par(mfrow = c(1,1), mar = c( 5, 5, 2, 2))
-# plot(cooks.distance(M3), xlim = c(0,10000), 
-#      ylim = c(0,1), xlab = "Observations", 
-#      ylab = " Cooks distance values")
-# abline(h = 1, lwd = 2, lty = 2)
-
-#not working!
-
-#6.if repeated measurements taken from the same site, check for patterns
-#not the case
-
-# #overdispersion
-
-E1 <- resid(M1, type = "pearson")
-sum(E1^2)/M1$df.residual
-
-#below 1
-
-#general
-gam.check(M1)
-summary(M1)
-
 
 #####Loop Model 2 ######
 
@@ -132,6 +40,7 @@ gam_output <- list()
 model_prediction <- list()
 derivatives <- list()
 viz <- list()
+pred_df <- list()
 
 #make new loop 
 
@@ -142,24 +51,29 @@ for (i in species_list) {
     from = min(data$mean_last_7days, na.rm = TRUE),
     to = max(data$mean_last_7days, na.rm = TRUE), by = 0.02))
   gam_output[[i]] <- gam(data = data, Abundance ~ s(mean_last_7days, k = 3), family = ziP())
-  viz[[i]] <- getViz(gam_output[[i]]) #needs to be in mgcviz class
-  print(plot(viz[[i]], allTerms = T), pages = 1)
-  print(qq(viz[[i]], rep = 20, showReps = T, CI = "none", a.qqpoi = list("shape" = 19), a.replin = list("alpha" = 0.2)))
-  tiff_filename <- paste("model_2/gam_check/gam_check_", i, ".tiff", sep = "")
-  tiff(tiff_filename, width = 800, height = 600)
-  print(check(viz[[i]],
-              a.qq = list(method = "simul1"),
-              a.respoi = list(size = 0.5),
-              a.hist = list(bins = 10)))
-  dev.off()
+  # viz[[i]] <- getViz(gam_output[[i]]) #needs to be in mgcviz class
+  # print(plot(viz[[i]], allTerms = T), pages = 1)
+  # print(qq(viz[[i]], rep = 20, showReps = T, CI = "none", a.qqpoi = list("shape" = 19), a.replin = list("alpha" = 0.2)))
+  # tiff_filename <- paste("model_2/gam_check/gam_check_", i, ".tiff", sep = "")
+  # tiff(tiff_filename, width = 800, height = 600)
+  # print(check(viz[[i]],
+  #             a.qq = list(method = "simul1"),
+  #             a.respoi = list(size = 0.5),
+  #             a.hist = list(bins = 10)))
+  # dev.off()
   # print(gam.check(gam_output[[i]]))
   # print(summary(gam_output[[i]]))
   print(tidy(gam_output[[i]]))
   # print(glance(gam_output[[i]]))
   model_prediction[[i]] <- predict.gam(gam_output[[i]], temp_gradient, type = "response", se.fit = TRUE)
-  model_bind <- cbind(model_prediction[[i]], temp_gradient) |>
+  model_bind <- cbind(model_prediction[[i]], temp_gradient)
+  pred_df <- model_bind |>
+    group_by(mean_last_7days) |>
+    mutate(fit = mean(fit)) |>
+    mutate(lower = fit - 2*se.fit, upper = fit + 2*se.fit) |>
+    summarize(fit = mean(fit), lower = mean(lower), upper = mean(upper), across(se.fit)) |>
     mutate(species = factor(i))
-  saveRDS(model_bind, paste0("model_2/predictions/predictions_",i,".rds"))
+  saveRDS(pred_df, paste0("model_2/predictions/predictions_",i,".rds"))
   # derivatives[[i]] <- derivatives(gam_output[[i]])
   # saveRDS(derivatives[[i]], paste0("model_2/derivatives/derivatives_", i, ".rds"))
 }
@@ -172,18 +86,22 @@ for (i in species_list) {
 
 s1 <- readRDS("model_2/predictions/predictions_Barbatula_sp_Lineage_II.rds")
 s2 <- readRDS("model_2/predictions/predictions_Coregonus_acrinasus.rds")
-#s3 <- readRDS("model_2/predictions/predictions_Coregonus_profundus.rds")
-#s4 <- readRDS("model_2/predictions/predictions_Coregonus_zugensis.rds")
+s3 <- readRDS("model_2/predictions/predictions_Coregonus_profundus.rds")
+s4 <- readRDS("model_2/predictions/predictions_Coregonus_zugensis.rds")
 s5 <- readRDS("model_2/predictions/predictions_Cottus_gobio_Profundal_Lucerne.rds")
 s6 <- readRDS("model_2/predictions/predictions_Cottus_gobio_Profundal_Thun.rds")
 s7 <- readRDS("model_2/predictions/predictions_Cottus_sp_Po_profundal.rds")
-#s8 <- readRDS("model_2/predictions/predictions_Phoxinus_sp.rds")
+s8 <- readRDS("model_2/predictions/predictions_Phoxinus_sp.rds")
 s9 <- readRDS("model_2/predictions/predictions_Telestes_muticellus.rds")
 s10 <- readRDS("model_2/predictions/predictions_Alosa_agone.rds")
 s11 <- readRDS("model_2/predictions/predictions_Cottus_sp_Po.rds")
 
-total_model_2_pred <- bind_rows(s1, s2, s5, s6, s7, s9, s10, s11) |> 
-  rename(prediction = fit, temp = mean_last_7days)
+total_model_2_pred <- bind_rows(s1, s2, s3, s4, s5, s6, s7, s8,  s9, s10, s11) |> 
+  rename(prediction = fit, temp = mean_last_7days) |> 
+  select(-`model_prediction[[i]]`)
+
+#save all predictions as RDS
+# saveRDS(total_model_2_pred, "total_models/total_model_2_pred")
 
 
 
@@ -261,14 +179,38 @@ total_model_2_pred |>
 mean_se_model_2 <- total_model_2_pred |> 
   group_by(species) |> 
   mutate(mean_se = mean(se.fit)) |> 
+  # mutate(across(where(is.numeric), ~ ifelse(is.na(.), 0, .))) |> 
   distinct(mean_se)
+
+
+summarize(n_lake = n_distinct(Lake))
 
 
 test2 <- df_abundance_gam |> 
   group_by(Species) |> 
-  count(Abundance) |> 
-  pivot_wider(names_from = Abundance, values_from = n) |> 
-  mutate(sumrow = rowSums(pick(3:8), na.rm = T)) |> 
-  select(-`2`, -`3`, -`4`, -`5`, -`7`, -`15`) |> 
-  rename(species = Species, observation_0 = `0`, observation_1 = `1`, observations_abu = `sumrow`)
+  mutate(total_abundance = sum(Abundance), 
+         observation_0 = sum(Abundance == 0)) |> 
+  distinct(total_abundance, observation_0) |> 
+  rename(species = Species) |> 
+  # pivot_wider(names_from = Abundance, values_from = n) |> 
+  # mutate(across(where(is.numeric), ~ ifelse(is.na(.), 0, .))) |> 
+  # rename(species = Species, observation_0 = `0`, observation_1 = `1`) |> 
+  mutate(n_lake = factor("1"))
+  # mutate(sumrow = rowSums(pick(3:8), na.rm = T)) |> 
+  # select(-`2`, -`3`, -`4`, -`5`, -`7`, -`15`) |> 
+  # rename(species = Species, observation_0 = `0`, observation_1 = `1`, observations_abu = `sumrow`)
   # rename(species = Species, observation_0 = `0`, observation_1 = `1`)
+
+#double check if all occur in one
+# n_lake <- df_abundance_gam |> 
+#   group_by(Lake, Species) |> 
+#   summarize(TotalAbundance = sum(Abundance), .groups = 'drop') |> 
+#   filter(TotalAbundance > 1) |> 
+#   group_by(Species) |> 
+#   summarize(n_lake = n_distinct(Lake)) |> 
+#   rename(species = Species)
+
+bind_2 <- merge(mean_se_model_2, test2)
+
+saveRDS(bind_2, "model_2/bind_2.rds")
+
