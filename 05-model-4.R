588bf6d18c848fe010e14c302e8d822e5adb3a91
Sophie Moreau <soanta.moreau@protonmail.com>
2023-11-15 17:17:08 +0100

    all dfs ncie and efficient

--- 05-model-4.R
+++ 05-model-4.R
@@ -117,45 +117,25 @@ for (i in species_list) {
 }
 
 
-#how can I check all the models easily??? some do not look good
-# checking out all plots
-#one big data frame and using facet_wrap
 
+# combined df
 
-s1 <- readRDS("model_4/predictions/predictions_Abramis_brama.rds")
-s2 <- readRDS("model_4/predictions/predictions_Alburnus_alburnus.rds")
-s3 <- readRDS("model_4/predictions/predictions_Alburnus_arborella.rds")
-s4 <- readRDS("model_4/predictions/predictions_Barbatula_sp_Lineage_I.rds")
-s5 <- readRDS("model_4/predictions/predictions_Blicca_bjoerkna.rds")
-s6 <- readRDS("model_4/predictions/predictions_Coregonus_albellus.rds")
-s7 <- readRDS("model_4/predictions/predictions_Coregonus_fatioi.rds")
-s8 <- readRDS("model_4/predictions/predictions_Coregonus_sarnensis.rds")
-s9 <- readRDS("model_4/predictions/predictions_Coregonus_sp.rds")
-s10 <- readRDS("model_4/predictions/predictions_Cyprinus_carpio.rds")
-s11 <- readRDS("model_4/predictions/predictions_Gasterosteus_aculeatus.rds")
-s12 <- readRDS("model_4/predictions/predictions_Gobio_gobio.rds")
-s13 <- readRDS("model_4/predictions/predictions_Gymnocephalus_cernua.rds")
-s14 <- readRDS("model_4/predictions/predictions_Lepomis_gibbosus.rds")
-s15 <- readRDS("model_4/predictions/predictions_Leuciscus_leuciscus.rds")
-s16 <- readRDS("model_4/predictions/predictions_Lota_lota.rds")
-s17 <- readRDS("model_4/predictions/predictions_Perca_fluviatilis.rds")
-s18 <- readRDS("model_4/predictions/predictions_Phoxinus_csikii.rds")
-s19 <- readRDS("model_4/predictions/predictions_Rutilus_rutilus.rds")
-s20 <- readRDS("model_4/predictions/predictions_Salmo_trutta.rds")
-s21 <- readRDS("model_4/predictions/predictions_Sander_lucioperca.rds")
-s22 <- readRDS("model_4/predictions/predictions_Scardinius_erythrophthalmus.rds")
-s23 <- readRDS("model_4/predictions/predictions_Scardinius_hesperidicus.rds")
-s24 <- readRDS("model_4/predictions/predictions_Tinca_tinca.rds")
+df_pred_mod4 <- list.files(path = "model_4/predictions", pattern = ".rds", full.names = TRUE) |> 
+  map_dfr(readRDS)
 
+# save total derivatives as RDS
+# saveRDS(df_pred_mod4, "total_models/pred_model_4_total")
 
-total_model_4_pred <- bind_rows(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10,  s11, s12,
-                                s13, s14, s15, s16, s17, s18, s19, s20, s21, s22, s23,
-                                s24) |> 
-  rename(prediction = fit)
-
-#save all predictions as RDS
-# saveRDS(total_model_4_pred, "total_models/total_model_4_pred")
-
+df_pred_mod4 |> 
+  filter(!species %in% c("Alburnus_arborella", "Cyprinus_carpio", "Salmo_trutta"
+                         , "Lepomis_gibbosus", "Phoxinus_csikii")) |>
+  ggplot(aes(temp, fit, color = factor(species))) +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.3) +
+  theme_bw() +
+  facet_wrap(~species) +
+  theme(strip.background = element_rect(fill="lightgrey")) +
+  scale_color_viridis(discrete=TRUE)
 
 # The reason we can't keep p-value < 2.2e-16 in the output is that that
 # would mean we'd need a character output rather than a numeric one, which 
@@ -177,6 +157,58 @@ total_model_4_pred <- bind_rows(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10,  s11, s
 # meaning that the actual p value is even smaller than 2.2e-16 
 # (a typical threshold is 0.05, anything smaller counts as statistically significant).
 # https://stats.stackexchange.com/questions/403343/what-is-the-interpretation-of-the-p-value-of-2-2e-16
+
+###################derivatives
+
+
+species_list <- df_abundance_re |> 
+  filter(!Species == "Lota_lota") |> 
+  distinct(Species) |> 
+  pull(Species)
+
+species_list <- sort(species_list)
+
+
+df_abundance_re$fLake <- as.factor(df_abundance_re$Lake)
+df_abundance_re$fProtocol <- as.factor(df_abundance_re$Protocol)
+
+str(df_abundance_re)
+
+# we need to get the derivatives for every lake
+
+derivatives <- list()
+gam_output <- list()
+
+for (i in species_list) {
+  data <- df_abundance_re |> 
+    filter(Species == i)
+  gam_output[[i]] <- gam(data = data, Abundance ~ s(mean_last_7days, k = 3) + s(fLake, bs = 're')
+                         +  s(fProtocol, bs = 're'), family = ziP())
+  #lota_lota
+  # gam_output[[i]] <- gam(data = data, Abundance ~ s(mean_last_7days, k = 6) + s(fLake, bs = 're')
+  # +  s(fProtocol, bs = 're'), family = ziP())
+  
+  lake_list <- distinct(data, Lake) |> 
+    pull()
+  for (j in lake_list){
+    derivatives <- derivatives(gam_output[[i]]) |> 
+      mutate(fLake = factor(j)) |>
+      mutate(species = factor(i)) |>
+      rename(temp = data)
+    saveRDS(derivatives, paste0("model_4/derivatives/derivatives_", i, "_",  j, ".rds"))
+  }
+}
+
+#prepare total df for derivatives
+
+df_deriv_mod4 <- list.files(path = "model_4/derivatives", pattern = ".rds", full.names = TRUE) |> 
+  map_dfr(readRDS)
+
+# save total derivatives as RDS
+# saveRDS(df_deriv_mod4, "total_models/deriv_model_4_total")
+
+
+
 
 # plot
 
@@ -230,46 +262,7 @@ two_bind4 <- merge(test4, n_lake4, by.x = "species")
 
 bind_4 <- merge(two_bind4, mean_se_model_4)
 
-saveRDS(bind_4, "model_4/bind_4.rds")
-
-# for one species to understand residuals
-df_one <- df_abundance_re |> 
-  filter(Species == "Perca_fluviatilis")
-
-df_one$fLake <- as.factor(df_one$Lake)
-
-df_one$fProtocol <- as.factor(df_one$Protocol)
-
-str(df_one)
-
-unique_lakes <- distinct(df_one, fLake)
-unique_protocol <- distinct(df_one, fProtocol)
-grid <- expand.grid(mean_last_7days = seq(
-  from = min(df_one$mean_last_7days, na.rm = TRUE),
-  to = max(df_one$mean_last_7days, na.rm = TRUE), by = 0.02
-), fLake = unique_lakes$fLake, fProtocol = unique_protocol$fProtocol)
-
-
-gam_output <- gam(data = df_one, Abundance ~ s(mean_last_7days, k = 3) + s(fLake, bs = "re")
-                       +  s(fProtocol, bs = 're'), family = ziP())
-
-
-# prepare residuals
-simulationOutput <- simulateResiduals(fittedModel = gam_output)
-
-# Main plot function from DHARMa, which gives 
-# Left: a qq-plot to detect overall deviations from the expected distribution
-# Right: a plot of the residuals against the rank-transformed model predictions
-plot(simulationOutput)
-
-# get rid of NAs in temp datat
-temp_data <- df_one |> 
-  drop_na(mean_last_7days)
-# Plotting standardized residuals against predictors
-plotResiduals(simulationOutput, temp_data$mean_last_7days, xlab = "temp", main=NULL)
-plotResiduals(simulationOutput, temp_data$fLake, xlab = "Lake", main=NULL)
-plotResiduals(simulationOutput, temp_data$fProtocol, xlab = "Protocol", main=NULL)
-
+# saveRDS(bind_4, "model_4/bind_4.rds")
 
 
 # these residuals are okay, see
