a36a6ca0ddc1604f41ed717edc7f964e7dd6242b
Sophie Moreau <soanta.moreau@protonmail.com>
2023-11-24 17:12:31 +0100

    depth test

--- 02-modelling.R
+++ 02-modelling.R
@@ -104,7 +104,96 @@ df_pred_mod1 |>
   theme(strip.background = element_rect(fill="lightgrey")) +
   scale_color_viridis(discrete=TRUE) 
 
+################################################3 Depth
 
+
+species_list <- df_binomial_gam |> 
+  distinct(Species) |> 
+  pull(Species)
+
+species_list <- sort(species_list)
+
+gam_output <- list()
+model_prediction <- list()
+derivatives <- list()
+pred_df <- list()
+grid <- list()
+unique_method <- list()
+simulationOutput <- list()
+tiff_file_2  <- list()
+depth_data <- list()
+
+
+df_binomial_gam$fProtocol <- as.factor(df_binomial_gam$Protocol)
+df_binomial_gam$fLake <- as.factor(df_binomial_gam$Lake)
+str(df_binomial_gam)
+#make new loop
+#adding fProtocol as random effect
+
+
+for (i in species_list) {
+  data <- df_binomial_gam |> 
+    filter(Species == i)
+  unique_method <- distinct(data, fProtocol)
+  grid <- expand.grid(Depth_sample = seq(
+    from = min(data$Depth_sample, na.rm = TRUE),
+    to = max(data$Depth_sample, na.rm = TRUE), by = 0.02),
+    fProtocol = unique_method$fProtocol)
+  gam_output[[i]] <- gam(data = data, Abundance ~ s(Depth_sample, k = 3) +
+                           s(fProtocol, bs = 're'), family = binomial)
+  
+  # prepare residuals
+  simulationOutput <- simulateResiduals(fittedModel = gam_output[[i]], plot = F)
+  # Main plot function from DHARMa, which gives 
+  # Left: a qq-plot to detect overall deviations from the expected distribution
+  # Right: a plot of the residuals against the rank-transformed model predictions
+  tiff_filename <- paste("model_1/depth/gam_check/gam_check_", i, ".tiff", sep = "")
+  tiff(tiff_filename, width = 800, height = 600)
+  print(plot(simulationOutput))
+  dev.off()
+  # get rid of NAs in temp datat
+  depth_data <- data |> 
+    drop_na(Depth_sample)
+  # Plotting standardized residuals against predictors
+  tiff_file_2 <- paste("model_1/depth/gam_check/predictor_", i, ".tiff", sep = "")
+  tiff(tiff_file_2, width = 800, height = 600)
+  print(plotResiduals(simulationOutput, depth_data$Depth_sample, xlab = "temp", main=NULL))
+  dev.off()
+  print(glance(gam_output[[i]]))
+  model_prediction[[i]] <- predict.gam(gam_output[[i]], grid, type = "response", se.fit = TRUE) #adding se, $fit
+  model_bind <- cbind(model_prediction[[i]], grid)
+  pred_df <- model_bind |>
+    group_by(Depth_sample) |>
+    mutate(fit = mean(fit)) |>
+    mutate(lower = fit - 2*se.fit, upper = fit + 2*se.fit) |>
+    summarize(fit = mean(fit), lower = mean(lower), upper = mean(upper),
+              across(se.fit), across(fProtocol)) |>
+    rename(depth = Depth_sample) |>
+    mutate(species = factor(i))
+  saveRDS(pred_df, paste0("model_1/depth/predictions/predictions_",i,".rds"))
+}
+
+# test <- readRDS("model_1/depth/predictions/predictions_Alosa_fallax.rds")
+# 
+# test |> 
+#   ggplot(aes(depth, fit)) +
+#   geom_line()
+
+depth_pred_mod1 <- list.files(path = "model_1/depth/predictions", pattern = ".rds", full.names = TRUE) |> 
+  map_dfr(readRDS)
+
+# save total derivatives as RDS
+# saveRDS(df_pred_mod1, "total_models/pred_model_1_total")
+
+
+depth_pred_mod1 |> 
+  ggplot(aes(depth, fit, color = factor(species))) +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.3) +
+  theme_bw() +
+  facet_wrap(~species, scales = "free") +
+  theme(strip.background = element_rect(fill="lightgrey")) +
+  scale_color_viridis(discrete=TRUE) 
 
 ###################derivatives
 
