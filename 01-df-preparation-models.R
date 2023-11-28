8373a18f6a26067f3beecedaa7ea8181a63e7937
Sophie Moreau <soanta.moreau@protonmail.com>
2023-11-28 15:38:58 +0100

    only coregonus_brienzii in lake brienz included

--- 01-df-preparation-models.R
+++ 01-df-preparation-models.R
@@ -12,13 +12,13 @@ df_final <- readRDS("df_final.rds")
 
 str(df_final)
 head(df_final)
-# 
-test <- df_final |>
-  filter(Species == "Coregonus_brienzii") |>
-  group_by(Lake) |>
-  summarize(sum(Presence))
-# 
+
+#
 # only include brienzii from lake brienz
+
+df_final <- df_final |> 
+  filter(!(Species == "Coregonus_brienzii" & Lake == "Thun"))
+  
 
 ###OVERVIEW
 #three problems: 
