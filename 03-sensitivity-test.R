e9a4ff1e8fb773db21f6a2d1bfb5ff86a8d20a3a
Sophie Moreau <soanta.moreau@protonmail.com>
2024-02-20 11:04:15 +0100

    only with mean depth of presence of species

--- 03-sensitivity-test.R
+++ 03-sensitivity-test.R
@@ -7,14 +7,12 @@ library(gt)
 library(mgcv)
 library(gratia)
 library(here)
-library(gamm4)
-library(broom)
 library(mgcViz)
 library(readxl)
-library(forcats)
 library(gridExtra)
 library(grid)
-library(gghighlight)
+library(broom)
+
 
 
 # wie hat Conor das gemeint mit tiefe konstant halten? Und wie kann ich diese mean_depth
@@ -37,27 +35,25 @@ df_sensitivity <- df_models |>
   filter(tot_abu > 300) |> 
   select(Lake, Species, Protocol, Depth_sample, Abundance, Presence, mean_last_7days) |> 
   #there are some NAs in depth -> drop them
-  drop_na(Depth_sample) |> 
-  group_by(Species) |> 
+  drop_na(Depth_sample, mean_last_7days)
   # we set depth to the mean of each species
-  mutate(mean_depth = mean(Depth_sample))
+  # mutate(mean_depth = mean(Depth_sample))
 
 df_sensitivity$fLake <- as.factor(df_sensitivity$Lake)
 df_sensitivity$fProtocol <- as.factor(df_sensitivity$Protocol)
-df_sensitivity$mean_depth <- as.numeric(df_sensitivity$mean_depth)
+# df_sensitivity$mean_depth <- as.numeric(df_sensitivity$mean_depth)
 
 
 species_list <- df_sensitivity |> 
   distinct(Species) |> 
   pull(Species)
-
-i <- "Alburnus_alburnus"
+# 
+# i <- "Alburnus_alburnus"
 for (i in species_list){
   
   data <- df_sensitivity |> 
     filter(Species == i)
   
-  str(data)
   # gam for temperature and depth with depth kept constant at mean depth ->
   # does not work we get error 
   model1 <- gam(data = data, Abundance ~ s(mean_last_7days, k = 3) + s(Depth_sample, k = 3)
@@ -73,13 +69,19 @@ for (i in species_list){
   
   random_lake <- sample(unique_lakes, 1)
   
+  # mean depth only in the depth where fish is present
+  mean_depth <- data |> 
+    filter(Presence == 1) |> 
+    mutate(mean_depth = mean(Depth_sample)) |> 
+    pull(mean_depth)
+
+  
   # depth sample is way too long, why??????
- not_workign <- expand.grid(mean_last_7days = seq(
+ grid1 <- expand.grid(mean_last_7days = seq(
     from = min(data$mean_last_7days, na.rm = TRUE),
     to = max(data$mean_last_7days, na.rm = TRUE), length = 1500),
     fProtocol = factor("VERT"), fLake = factor(random_lake), 
-    Depth_sample = seq(from = min(data$mean_depth),
-                       to = max(data$mean_depth), length = 1500))
+    Depth_sample = mean_depth)
   
   grid2 <- expand.grid(mean_last_7days = seq(
     from = min(data$mean_last_7days, na.rm = TRUE),
@@ -93,36 +95,41 @@ for (i in species_list){
                                   exclude = c("s(fProtocol)", "s(fLake)"), 
                                   type = "response", se.fit = TRUE)
   
-  model_bind1 <- cbind(grid1, as.data.frame(model_prediction1))
+  model_bind1 <- cbind(grid1, as.data.frame(model_prediction1)) |> 
+    # rename(fit1 = fit) |> 
+    # rename(se.fit1 = se.fit) |> 
+    # rename(temp = mean_last_7days) |> 
+    mutate(model = factor("temp_and_depth")) |> 
+    select(-Depth_sample)
   
   
   model_prediction2 <- predict.gam(model2, newdata = grid2,
                                    exclude = c("s(fProtocol)", "s(fLake)"), 
                                    type = "response", se.fit = TRUE)
   
-  model_bind2 <- cbind(grid2, as.data.frame(model_prediction2))
+  model_bind2 <- cbind(grid2, as.data.frame(model_prediction2)) |> 
+    mutate(model = factor("temp")) 
   
-  # pred_df1 <- model_bind1 |>
-  #   rename(temp = mean_last_7days) |> 
-  #   mutate(species = factor(i))
-  # 
-  # summary <- summary(gam_output1)
-  # 
-  # 
-  # 
-  # plot_pred1 <- pred_df1 |>
-  #   ggplot(aes(temp, fit)) +
-  #   geom_line() +
-  #   geom_ribbon(aes(ymin = (fit - se.fit), ymax = (fit + se.fit)), alpha = 0.3) +
-  #   theme_bw() +
-  #   # facet_wrap(~fLake, scale = "free") +
-  #   theme(strip.background = element_rect(fill="lightgrey")) +
-  #   labs(title = paste("Species = ", i,
-  #                      "deviance explained = ", signif(summary[["dev.expl"]])))
+  pred_df <- rbind(model_bind1, model_bind2) |> 
+    rename(temp = mean_last_7days) |> 
+    select(-fProtocol, -fLake)
   
+
+  plot_pred <- pred_df |>
+    ggplot(aes(temp, fit, color = model, group = model)) +
+    geom_line() +
+    geom_ribbon(aes(ymin = (fit - se.fit), ymax = (fit + se.fit), fill  = model),  alpha = 0.2) +
+    theme_bw() +
+    labs(title = paste("Sensitivity test", i)) +
+    scale_color_manual(values = c("#01665E", "#D73027"), aesthetics = c("color", "fill"))
   
-  # gam(y ~ s(temp) + s(depth) + re) -> depth constant at mean depth of species and gam(y ~ s(temp) +re)
-  # plot predictions of both in one plot and look at differences
+  plot_pred
+  
+  tiff(paste("total_models/sensitivity/sensitivity_test_", i ,".tiff", sep = ""), units="in", width=9, height=6, res=300)
+  
+  print(plot(plot_pred))
+  
+  dev.off()
   # rescale() after predictions to account for differences
   
 }
